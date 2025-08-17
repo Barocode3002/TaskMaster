0762de68cbf6b397f16a8a1879737092e50958e7
Mohsen Barokha <mohsenbarokha@192.168.1.17>
2025-08-17 16:50:28 +0300

    chore(footer): inline icon and link/style tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import * as styles from './styles/footerStyle';
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { TiSocialFacebookCircular } from "react-icons/ti";
+import { FaCodeBranch } from "react-icons/fa6";
 
 
 const Footer = () => {
@@ -11,15 +12,23 @@ const Footer = () => {
             </p>
             <div className={styles.socialLinksContainer}>
                 <a href="https://fb.com/mohssenmohammedbarokha" className={styles.socialLink}><TiSocialFacebookCircular /></a>
-                <a href="https://instagram.com/mohssenmbarokha" className={styles.socialLink}><FaInstagram /></a>
-                <a href="https://github.com/Barocode3002" className={styles.socialLink}><FaGithub /></a>
-                <a href="https://linkedin.com/in/mohssenmbarokha" className={styles.socialLink}><FaLinkedin /></a>
+                <a href="https://instagram.com/mohssen_barokha" className={styles.socialLink}><FaInstagram /></a>
+                <a href="https://github.com/mohssenbarokha" className={styles.socialLink}><FaGithub /></a>
+                <a href="https://linkedin.com/in/mohssen-mohammed-barokha-a5b1b8230" className={styles.socialLink}><FaLinkedin /></a>
             </div>
-            <div className='m-2 hover:text-red-500 cursor-pointer'>
-            <a href="https://github.com/Barocode3002/TaskMaster">source code</a>
+            <div className={styles.projectSourceContainer}>
+            <a href="https://github.com/Barocode3002/TaskMaster" className={styles.projectSourceLink}>
+                <span className={styles.projectSourceText}>
+                    <code>
+                        <span className={styles.inlineIcon}><FaCodeBranch /></span>source code
+                    </code>
+                </span>
+            </a>
             </div>
         </footer>
     );
 };
 
 export default Footer;
+
+// ___UPDATED___
--- src/components/styles/footerStyle.tsx
+++ src/components/styles/footerStyle.tsx
@@ -2,4 +2,11 @@
 export const footerContainer = "py-4 px-4 text-center dark:bg-stone-800";
 export const copyrightText = "text-xs text-gray-400 mb-2 dark:text-stone-400";
 export const socialLinksContainer = "flex justify-center space-x-4";
-export const socialLink = "text-gray-400 hover:text-gray-600 transition-colors duration-200 text-lg dark:text-stone-400 dark:hover:text-stone-300";
+export const socialLink = "text-gray-400 hover:text-red-600 transition-colors duration-200 text-lg dark:text-stone-400 dark:hover:text-gray-300";
+export const projectSourceContainer = "flex justify-center space-x-4 mt-2 mt-5";
+export const projectSourceLink = "overflow-hidden relative inline-flex justify-center items-center px-2 py-1 text-gray-400 hover:text-red-600 transition-colors duration-200 text-lg dark:text-stone-400 dark:hover:text-red-300";
+// Background icon no longer needed when embedding in word; keep hidden to avoid bleed-through
+export const projectSourceIcon = "hidden";
+export const projectSourceText = "relative z-10";
+// Inline icon used to replace a character inside the word 'source'
+export const inlineIcon = "inline-block align-[0.000001em] mx-1 text-[1em] leading-none";
