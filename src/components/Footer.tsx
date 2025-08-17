f97c74295873bcc42693f35277efdd76c4c7ca97
Mohsen M Barokha (barocode) <107206086+Barocode3002@users.noreply.github.com>
2025-08-17 15:56:50 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import * as styles from './styles/footerStyle';
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaInstagram, FaLinkedin, FaCodeMerge } from "react-icons/fa";
 import { TiSocialFacebookCircular } from "react-icons/ti";
 
 
@@ -11,12 +11,12 @@ const Footer = () => {
             </p>
             <div className={styles.socialLinksContainer}>
                 <a href="https://fb.com/mohssenmohammedbarokha" className={styles.socialLink}><TiSocialFacebookCircular /></a>
-                <a href="https://instagram.com/mohssen_barokha" className={styles.socialLink}><FaInstagram /></a>
-                <a href="https://github.com/mohssenbarokha" className={styles.socialLink}><FaGithub /></a>
-                <a href="https://linkedin.com/in/mohssen-mohammed-barokha-a5b1b8230" className={styles.socialLink}><FaLinkedin /></a>
+                <a href="https://instagram.com/mohssenmbarokha" className={styles.socialLink}><FaInstagram /></a>
+                <a href="https://github.com/Barocode3002" className={styles.socialLink}><FaGithub /></a>
+                <a href="https://linkedin.com/in/mohssenmbarokha" className={styles.socialLink}><FaLinkedin /></a>
             </div>
             <div className='m-2 hover:text-red-500 cursor-pointer'>
-            <a href="https://github.com/Barocode3002/TaskMaster"><code>source code</code></a>
+            <a href="https://github.com/Barocode3002/TaskMaster"><FaCodeMerge /><code>source code</code></a>
             </div>
         </footer>
     );
